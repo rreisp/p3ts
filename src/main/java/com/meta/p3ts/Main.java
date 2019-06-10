3337f0e472d634377c029ffc8781b68b709e8de7
Rodrigo Reis <rreisp@gmail.com>
2019-06-10 15:21:13 -0300

    Criando operação de instanciação

--- src/main/java/com/meta/p3ts/Main.java
+++ src/main/java/com/meta/p3ts/Main.java
@@ -1,13 +1,31 @@
 package com.meta.p3ts;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.meta.p3ts.domain.Categoria;
+import com.meta.p3ts.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class Main {
+public class Main implements CommandLineRunner {
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
