5816fc27c41875ccd7e9d9ae59052c4508ed8aa3
Rodrigo Reis <rreisp@gmail.com>
2019-06-12 11:04:23 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/meta/p3ts/services/CategoriaService.java
+++ src/main/java/com/meta/p3ts/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.meta.p3ts.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.meta.p3ts.domain.Categoria;
 import com.meta.p3ts.repositories.CategoriaRepository;
+import com.meta.p3ts.services.exceptions.DataIntegrityException;
 import com.meta.p3ts.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 }
