29c526720cde4eb59230bf0677436973080ebf4c
Rodrigo Reis <rreisp@gmail.com>
2019-06-24 16:58:10 -0300

    Email HTML

--- src/main/java/com/meta/p3ts/services/EmailService.java
+++ src/main/java/com/meta/p3ts/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.meta.p3ts.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.meta.p3ts.domain.Pedido;
@@ -9,4 +11,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
