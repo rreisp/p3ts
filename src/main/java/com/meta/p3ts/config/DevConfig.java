beee2cce5901298b8487a786511cdf57904c8fc0
Rodrigo Reis <rreisp@gmail.com>
2019-06-24 16:30:57 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/meta/p3ts/config/DevConfig.java
+++ src/main/java/com/meta/p3ts/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.meta.p3ts.services.DBService;
+import com.meta.p3ts.services.EmailService;
+import com.meta.p3ts.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -28,5 +30,10 @@ public class DevConfig {
 		
 		dbService.instantiateTestDatabase();
 		return true;
-	} 
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
 }
