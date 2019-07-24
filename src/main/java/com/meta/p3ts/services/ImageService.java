493cc33d7559d88217bb36348ee970658f043913
Rodrigo Reis <rreisp@gmail.com>
2019-07-24 15:55:42 -0300

    Bônus: ajustando tamanho da imagem

--- src/main/java/com/meta/p3ts/services/ImageService.java
+++ src/main/java/com/meta/p3ts/services/ImageService.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FilenameUtils;
+import org.imgscalr.Scalr;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -25,7 +26,7 @@ public class ImageService {
 
 		try {
 			BufferedImage img = ImageIO.read(uploadedFile.getInputStream());
-			if("png".equals(ext)) {
+			if ("png".equals(ext)) {
 				img = pngToJpg(img);
 			}
 			return img;
@@ -36,10 +37,10 @@ public class ImageService {
 
 	public BufferedImage pngToJpg(BufferedImage img) {
 		BufferedImage jpgImage = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
-		jpgImage.createGraphics().drawImage(img, 0, 0 , Color.WHITE, null);
+		jpgImage.createGraphics().drawImage(img, 0, 0, Color.WHITE, null);
 		return jpgImage;
 	}
-	
+
 	public InputStream getInputStream(BufferedImage img, String extension) {
 		try {
 			ByteArrayOutputStream os = new ByteArrayOutputStream();
@@ -47,6 +48,20 @@ public class ImageService {
 			return new ByteArrayInputStream(os.toByteArray());
 		} catch (IOException e) {
 			throw new FileException("Erro ao ler arquivo");
-		}		
+		}
+	}
+
+	public BufferedImage cropSquare(BufferedImage sourceImg) {
+		int min = (sourceImg.getHeight() <= sourceImg.getWidth()) ? sourceImg.getHeight() : sourceImg.getWidth();
+		return Scalr.crop(
+				sourceImg,
+				(sourceImg.getWidth() / 2) - (min / 2),
+				(sourceImg.getHeight() / 2) - (min / 2),
+				min,
+				min);
+	}
+	
+	public BufferedImage resize(BufferedImage sourceImg, int size) {
+		return Scalr.resize(sourceImg, Scalr.Method.ULTRA_QUALITY, size);
 	}
 }
